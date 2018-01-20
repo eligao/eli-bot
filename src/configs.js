cf4e230b5029eeaf24f93c7e1a81cb5e380cc561
Eli Gao <refraction222@gmail.com>
2018-01-20 13:35:50 +0800

    add missing configs entry

--- src/configs.js
+++ src/configs.js
@@ -3,7 +3,8 @@ const configs = {
     BOT_USERNAME: process.env.BOT_USERNAME,
     BOT_WEBHOOK_HOST: process.env.BOT_WEBHOOK_HOST,
     BOT_WEBHOOK_PORT: process.env.BOT_WEBHOOK_PORT || process.env.PORT || 3023,
-    BOT_LOGGER_CHANNEL_ID: process.env.BOT_LOGGER_CHANNEL_ID
+    BOT_LOGGER_CHANNEL_ID: process.env.BOT_LOGGER_CHANNEL_ID,
+    BOT_HUOBI_OTC_CHANNEL_ID: process.env.BOT_HUOBI_OTC_CHANNEL_ID,
 };
 
 module.exports = configs;
